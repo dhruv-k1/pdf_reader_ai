0fc877bf90f704ed8c3911382df80e97439c53d3
Dhruv Khandelwal <128924632+dhruv-k1@users.noreply.github.com>
2024-06-21 00:03:10 +0530

    update

--- query_model.py
+++
@@ -1,9 +0,0 @@
-from bert_model import preprocess_text
-
-def get_response(model, query):
-    query_tokens = preprocess_text(query)
-    try:
-        responses = model.wv.most_similar(positive=query_tokens, topn=1)
-        return responses[0][0] if responses else "Sorry, I don't have an answer for that."
-    except KeyError:
-        return "Sorry, I don't understand the question."
